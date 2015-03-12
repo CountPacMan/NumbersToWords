c4583dfce6c6a75b575e64c68a162294da58bd96
Daniel Toader & Patrick Fiorentino <student@epicodus.com>
2015-03-12 14:04:29 -0700

    redo class logic to account for decimal number system position

--- src/NumbersToWords.php
+++ src/NumbersToWords.php
@@ -7,19 +7,13 @@
         $values = ['0' => 'zero', '1' => "one", '2' => "two", '3' => "three", '4' => "four", 'five' => "five", '6' => "six", '7' => "seven", '8' => "eight", '9' => "nine", '10' => "ten"];
 
         $num_array = str_split($numbers);
-        foreach ($num_array as $number) {
-          foreach ($values as $key => $value) {
-            // echo "first base ";
-            // echo "<p> key is " . $key . "</p>";
-            // echo "<p> number is " . $number . "</p>";
-            // echo "<p> value is " . $value . "</p>";
-            if ($number == $key) {
-              // echo "entered! ";
-              $final .= $value;
-            }
+        $position = count($num_array) - 1;
+        for ($position; $position >= 0; $position--) {
+          $number = $num_array[$position];
+          if ($position == 0) {
+            $final .= $values[$number];
           }
         }
-        // echo $final;
         return $final;
       }
     }
@@ -27,3 +21,8 @@
     // $test = new NumbersToWords;
     // $test->convert(1);
  ?>
+
+
+ <!-- elseif ($number == $key) {
+   // echo "entered! ";
+   $final = $value . $final; -->
